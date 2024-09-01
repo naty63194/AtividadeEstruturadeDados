0f553f1267e124bb712fde62a5156751544305bf
GabrielInada <gabrieljunichiro2@gmail.com>
2024-09-01 18:27:42 -0300

    Corpo inicial do menu
    O menu está montado mas efetivamente falando ele não faz nada, já estou implementando as opções

--- main.py
+++ main.py
@@ -129,21 +129,65 @@ class ListaDuplamenteEncadeadaOrdenada:
 
         return f"O número {valor} não foi encontrado."
 
+# Função que chama o menu
+def menu():
+    try:
+        lista = ListaDuplamenteEncadeadaOrdenada()
+    except Exception:
+        print("\033[31mERRO Algo deu errado! Verifique o código!\033[m")
+    else:
+        print("Lista Inicializada com sucesso!")
+        while True:
+            print(f'{" Menu ":=^40}')
+            print("1. Mostrar lista (Crescente)")
+            print("2. Mostrar lista (Decrescente)")
+            print("3. Inserir um valor")
+            print("4. Excluir um valor (Início)")
+            print("5. Excluir um valor (Final)")
+            print("6. Excluir um valor (Tudo)")
+            print("7. Buscar um valor (Crescente)")
+            print("8. Buscar um valor (Decrescente)")
+            print("9. Sair")
+            escolha = int(input("Escolha: "))
+
+            if escolha == 1:
+                print("escolheu 1")
+            elif escolha == 2:
+                print("escolheu 2")
+            elif escolha == 3:
+                print("escolheu 3")
+            elif escolha == 4:
+                print("escolheu 4")
+            elif escolha == 5:
+                print("escolheu 5")
+            elif escolha == 6:
+                print("escolheu 6")
+            elif escolha == 7:
+                print("escolheu 7")
+            elif escolha == 8:
+                print("escolheu 8")
+            else:
+                break
 
 
-
+menu()
 
 # Exemplo de uso:
-lista = ListaDuplamenteEncadeadaOrdenada()
-lista.insere_ordenado(10)
-lista.insere_ordenado(20)
-lista.insere_ordenado(30)
-lista.insere_ordenado(40)
+# lista = ListaDuplamenteEncadeadaOrdenada()
+# lista.insere_ordenado(10)
+# lista.insere_ordenado(20)
+# lista.insere_ordenado(30)
+# lista.insere_ordenado(40)
+# lista.insere_ordenado(40)
+# lista.insere_ordenado(80)
+# lista.insere_ordenado(5)
+# lista.insere_ordenado(35)
+# lista.insere_ordenado(55)
 
-lista.mostrar_frente()
-
-print("")
-
-print(lista.buscar_frente(30))
-print(lista.buscar_tras(30))
+# lista.mostrar_frente()
+#
+# print("")
+#
+# print(lista.buscar_frente(30))
+# print(lista.buscar_tras(30))
 
